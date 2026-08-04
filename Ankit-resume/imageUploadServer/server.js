ec8c746b3f2ca769cd77db85889f9249f9c5ea8e
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-04 23:49:50 +0530

    Update server.js

--- Ankit-resume/imageUploadServer/server.js
+++ Ankit-resume/imageUploadServer/server.js
@@ -2,7 +2,7 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
+
 
 
 import certificateRoutes from "./routes/certificateRoutes.js";
@@ -17,12 +17,7 @@ const app = express();
 
 // Middleware
 
-app.use(
-  cors({
-    origin: "https://ankit-resume-tejg-brown.vercel.app/",
-    credentials: true,
-  })
-);
+
 
 
 app.use(express.json());
