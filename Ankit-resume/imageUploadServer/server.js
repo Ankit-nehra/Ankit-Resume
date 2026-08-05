cd5bec800503fd3f6793b3bfbb90771afc8921f6
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-05 07:11:06 +0530

    Refactor server.js to streamline code structure

--- Ankit-resume/imageUploadServer/server.js
+++ Ankit-resume/imageUploadServer/server.js
@@ -1,32 +1,13 @@
-// server.js
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
-
 
 import certificateRoutes from "./routes/certificateRoutes.js";
 
-
-
-
-
 const app = express();
 
-
-
 // Middleware
-
-app.use(
-  cors({
-    origin: "https://ankit-resume-teal.vercel.app/",
-    credentials: true,
-  })
-);
-
-
 app.use(express.json());
-
 
 app.use(
   express.urlencoded({
@@ -34,97 +15,50 @@ app.use(
   })
 );
 
-
-
 // MongoDB Connection
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-
     console.log("✅ MongoDB Connected");
-
   })
   .catch((err) => {
-
-    console.log(
-      "MongoDB Error:",
-      err.message
-    );
-
+    console.log("MongoDB Error:", err.message);
   });
 
-
-
-
 // Routes
-
-app.use(
-  "/api/certificates",
-  certificateRoutes
-);
-
-
-
+app.use("/api/certificates", certificateRoutes);
 
 // Test Routes
-
 app.get("/", (req, res) => {
-
-  res.send(
-    "API is Running..."
-  );
-
+  res.send("API is Running...");
 });
 
-
 app.get("/api/test", (req, res) => {
-
   res.json({
     message: "Certificate API Working",
   });
-
 });
 
-app.post("/api/verify-upload", (req,res)=>{
+app.post("/api/verify-upload", (req, res) => {
+  const { key } = req.body;
 
+  console.log(key);
 
-    const {key}=req.body;
-console.log(key);
-
-    if(key === process.env.UPLOAD_SECRET){
-
-
-        return res.json({
-            success:true
-        });
-
-
-    }
-
-
-
-    return res.status(401).json({
-
-        success:false,
-        message:"Invalid Secret Key"
-
+  if (key === process.env.UPLOAD_SECRET) {
+    return res.json({
+      success: true,
     });
+  }
 
-
+  return res.status(401).json({
+    success: false,
+    message: "Invalid Secret Key",
+  });
 });
-
-
 
 // Server Start
-
 const PORT = process.env.PORT || 3001;
 
-
 app.listen(PORT, () => {
-
-  console.log(
-    `🚀 Server running on port ${PORT}`
-  );
-
+  console.log(`🚀 Server running on port ${PORT}`);
 });
