06bdbb21a125a549e49af91814be3a8d2d39bae5
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-06 16:06:21 +0530

    Update Profile.jsx

--- Ankit-resume/src/screens/Profile.jsx
+++ Ankit-resume/src/screens/Profile.jsx
@@ -126,7 +126,7 @@ I continuously learn new technologies, research better approaches and focus on w
         "2024 - 2026",
 
         score:
-        "Percentage: 87.85"
+        "Percentage: 87.85%"
 
       },
 
@@ -143,7 +143,7 @@ I continuously learn new technologies, research better approaches and focus on w
         "2021 - 2024",
 
         score:
-        "Percentage: 73.97"
+        "Percentage: 73.97%"
 
       }
 
