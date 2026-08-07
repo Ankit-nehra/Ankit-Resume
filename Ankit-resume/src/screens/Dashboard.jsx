f387392139a2cbd7877d800b315bebb867737b80
Ankit-nehra <ankitnehra7000@gmail.com>
2026-08-07 14:53:42 +0530

    Update Dashboard.jsx

--- Ankit-resume/src/screens/Dashboard.jsx
+++ Ankit-resume/src/screens/Dashboard.jsx
@@ -58,7 +58,7 @@ export default function Dashboard({ lightMode, setLightMode }) {
 
 
 
-const resumeLink = "https://docs.google.com/document/d/1zbNBpTCven3Q8-BZcbY5BqYJELCNK4B7gBH-NZIfOHM/edit?tab=t.0";
+const resumeLink = "https://res.cloudinary.com/dhslfpaio/raw/upload/v1786094376/Resume_1_bwja1a.docx";
   // Add your document URL here
 
 
